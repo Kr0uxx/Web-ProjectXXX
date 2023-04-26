164b411db77e16214d1bf9e75e7c9984848142a9
Katiapater <katiasafronova08@yandex.ru>
2023-04-26 10:10:09 +0300

    determining the address by coordinates or geolocation

---
+++ functions/map_func.py
@@ -0,0 +1,48 @@
+import requests
+
+
+# from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# функция для получения адреса по координатам
+def get_address_from_coords(coords):
+    parametrs = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "format": "json",
+        "lang": "ru_RU",
+        "kind": "house",
+        "geocode": coords
+    }
+
+    try:
+        r = requests.get(url="https://geocode-maps.yandex.ru/1.x/", params=parametres)
+        json_data = r.json()
+        address_str = json_data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["metaDataProperty"][
+            "GeocoderMetaData"]["AddressDetails"]["Country"]["AddressLine"]
+        return address_str
+
+    except Exception:
+        return "Не могу определить адрес по введенным данным.\n\nОтправь мне свою геолокацию или координаты в " \
+               "формате (долгота, широта) "
+
+
+# Нужно, чтобы бот отправил такое сообщение в самом начале при кнопке карты
+# Отправь мне свою геолокацию или координаты в формате (долгота, широта)
+
+# функция, если пользователь отправил координаты
+def text(coordinates):
+    # получение адреса от пользователя
+    coords = coordinates
+    # получение адреса из координат
+    address_str = get_address_from_coords(coords)
+    return address_str
+
+
+# функция, если пользователь отправил геолокацию
+def location(geolocation):
+    message = geolocation
+    # получение долготы и широты
+    current_position = (message.location.longitude, message.location.latitude)
+    coords = f"{current_position[0]},{current_position[1]}"
+    # отправляем координаты в нашу функцию получения адреса
+    address_str = get_address_from_coords(coords)
+    return address_str
